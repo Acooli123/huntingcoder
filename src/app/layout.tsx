04e85dc2ad0e7b74bfdd96e74ffa0756148221df
Acooli123 <soumyajitacooli@gmail.com>
2025-08-15 22:41:28 +0530

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,16 @@
-// src/app/layout.js
 import './globals.css';
 import Navbar from './components/Navbar';
-import React from 'react';
+import React, { ReactNode } from 'react';
 
 export const metadata = {
   title: 'Hunting Coder',
   description: 'A blog for hunting coders by a coder',
 };
 
-// ✅ MUST use <html> and <body> inside a Server Component
-// ✅ 'children' is passed to the layout component
-// ✅ Must use the `Layout` function as default export with correct typing
-
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className="pt-16"> {/* Add padding if Navbar is fixed */}
+      <body className="pt-16">
         <Navbar />
         <main>{children}</main>
       </body>
